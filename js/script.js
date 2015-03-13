f81190d7e251524b5fe719f43dcf94bf427aa2b6
hyounmin <hyunmin90@gmail.com>
2015-03-13 07:21:58 +0000

    final

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-var divs = document.getElementsByClassName('alert');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
-}
 
-function highlightThis(event) {
-    //event.stopPropagation();
-  
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
-}
+
+
+$('.fade').slick({
+  infinite:true,
+  slidesToShow:3,
+  cssEase: 'linear',
+});    
+    
+$(function() {
+  $(document).foundation();
+});
+
+
+clickedScroll= function(id){
+  $('html, body').animate({
+    scrollTop: $("#"+id).offset().top
+  }, 2000); 
+};
